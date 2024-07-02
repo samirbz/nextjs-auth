b97489ea90b4db7a165f13b8936f9776d5ab3c4d
samir <febsec14@gmail.com>
2024-07-02 23:01:41 +0545

    forgot password page created

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,7 +1,48 @@
-import React from "react"
+"use client"
+import axios from "axios"
+import Link from "next/link"
+import React, { useState } from "react"
+import toast from "react-hot-toast"
 
 const ForgotPassword = () => {
-  return <div>page</div>
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  })
+
+  const onResetPassword = async () => {
+    try {
+      const response = await axios.post("/api/users/sign-in", user)
+      console.log("Login success", response.data)
+      toast.success("Login success")
+      // router.push("/")
+      location.assign("/")
+    } catch (error: any) {
+      console.log("Login failed", error.message)
+      toast.error(error.message)
+    }
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <label htmlFor="email">email</label>
+      <input
+        className="mb-4 rounded-lg border border-gray-300 p-2 text-black focus:border-gray-600 focus:outline-none"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+
+      <button
+        onClick={onResetPassword}
+        className="mb-4 rounded-lg border border-gray-300 p-2 focus:border-gray-600 focus:outline-none"
+      >
+        Reset password
+      </button>
+    </div>
+  )
 }
 
 export default ForgotPassword
